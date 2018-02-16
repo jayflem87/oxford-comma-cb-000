6460be0c5985345e654143251ca2c8fee7a7c900
Jay Fleming <jacobmfleming@gmail.com>
2018-02-16 18:30:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,7 @@
 def oxford_comma(array)
-  string = array.split(",")
   last_item = array.pop
-  array_length = array.length
-  while array.length > 1
-    array.each |item|
+  string = array.split(",")
 
-    end
+
   end
 end
